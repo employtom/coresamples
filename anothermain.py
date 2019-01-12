8987f92e5680bae7e94f440394f459e247834a57
Tom Moody <tom.moody@gmail.com>
2019-01-11 20:27:33 -0700

    commit

---
+++ anothermain.py
@@ -0,0 +1,17 @@
+'''
+Created on Jan 11, 2019
+
+@author: tommoody
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
